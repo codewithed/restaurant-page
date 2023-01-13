1c31a64fb5b4635559c47c116b5447de938caee3
Edmund Debrah <edmundking05@gmail.com>
2023-01-13 19:12:25 +0000

    update styling

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,6 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-alert('I am happy with my progress 🥳');
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImFsZXJ0KCdJIGFtIGhhcHB5IHdpdGggbXkgcHJvZ3Jlc3Mg8J+lsycpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
