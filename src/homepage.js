ea71dce6413a3372a3715268387fde9f9e26fc4b
Edmund Debrah <edmundking05@gmail.com>
2023-01-13 19:02:31 +0000

    add background image and style header

--- src/homepage.js
+++
@@ -1,5 +0,0 @@
-const home = document.createElement('div');
-home.innerHTML = "<p>Best Ramen in the hidden leaf </p>
-<p>Made with passion since the first great ninja war!</p>
-<img src="/images/teuchi.png" alt="Sage of six bowls">
-<p>Order online or visit us!</p>";
