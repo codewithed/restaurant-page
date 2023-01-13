f5d44e463f81424669e29f34654d300a7e0877a2
Edmund Debrah <edmundking05@gmail.com>
2023-01-13 18:22:25 +0000

    add initial page markup

---
+++ src/homepage.js
@@ -0,0 +1,5 @@
+const home = document.createElement('div');
+home.innerHTML = "<p>Best Ramen in the hidden leaf </p>
+<p>Made with passion since the first great ninja war!</p>
+<img src="/images/teuchi.png" alt="Sage of six bowls">
+<p>Order online or visit us!</p>";
--- src/index.js
+++
@@ -1,1 +0,0 @@
-alert('I am happy with my progress 🥳');
