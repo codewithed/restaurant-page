3bdaf69e63f2d8652ff9878e482905609860b8a0
Edmund Debrah <edmundking05@gmail.com>
2023-01-16 17:06:45 +0000

    add event listeners to  contact and menu buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import loadHomePage from './homepage';
+import loadContactPage from './contact';
+import loadMenuPage from './menu';
 
 loadHomePage();
 
 const homeBtn = document.getElementById('homeBtn');
 homeBtn.addEventListener('click', loadHomePage);
+
+const menuBtn = document.getElementById('menuBtn');
+menuBtn.addEventListener('click', loadContactPage);
+
+const contactBtn = document.getElementById('contactBtn');
+contactBtn.addEventListener('click', loadMenuPage);
