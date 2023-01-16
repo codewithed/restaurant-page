2565d2f91c301477358ba20c16ab9efde30ed3ef
Edmund Debrah <edmundking05@gmail.com>
2023-01-16 18:22:19 +0000

    finish contact page

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,9 @@
 function loadContactPage() {
   const main = document.getElementById('main');
-  main.innerHTML = '<div class="contact" id="contact"></div>';
+  main.innerHTML = `<div class="contact" id="contact">
+  <img src="/images/konoha.jpeg" alt="Location: The hidden leaf village">
+  <p class="contactInfo">Visit us at The Hidden Leaf Village (Konohagakure) or order online!</p>
+  </div>`;
 }
 
 export default (loadContactPage);
