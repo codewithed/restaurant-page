3e88e2872d516aebff672a68309c3d6f0326ee99
Edmund Debrah <edmundking05@gmail.com>
2023-01-16 17:01:39 +0000

    add initial template for contact.js and menu.js

---
+++ src/contact.js
@@ -0,0 +1,6 @@
+function loadContactPage() {
+  const main = document.getElementById('main');
+  main.innerHTML = '<div class="contact" id="contact"></div>';
+}
+
+export default (loadContactPage);
---
+++ src/menu.js
@@ -0,0 +1,6 @@
+function loadMenuPage() {
+  const main = document.getElementById('main');
+  main.innerHTML = '<div class="menu" id="menu"></div>';
+}
+
+export default (loadMenuPage);
