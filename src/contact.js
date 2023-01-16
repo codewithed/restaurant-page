d35233c7c98641f859f2fc5c606182ddf690a0fa
Edmund Debrah <edmundking05@gmail.com>
2023-01-16 22:23:52 +0000

    change image path

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,7 @@
 function loadContactPage() {
   const main = document.getElementById('main');
   main.innerHTML = `<div class="contact" id="contact">
-  <img src="/images/konoha.jpeg" alt="Location: The hidden leaf village">
+  <img src="images/konoha.jpeg" alt="Location: The hidden leaf village">
   <p class="contactInfo">Visit us at The Hidden Leaf Village (Konohagakure) or order online!</p>
   </div>`;
 }
